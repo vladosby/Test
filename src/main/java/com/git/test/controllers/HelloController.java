406268dcc958360d48b521f5b486967738dc2592
Vladislav Kievski <V.Kievskij@bssys.com>
2014-11-26 13:39:13 +0300

    Conrigured Spring MVC

---
+++ src/main/java/com/git/test/controllers/HelloController.java
@@ -0,0 +1,27 @@
+package com.git.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.jms.ConnectionFactory;
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.MessageProducer;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+//Import the classes to use JNDI.
+
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+        return "hello";
+
+	}
+}
